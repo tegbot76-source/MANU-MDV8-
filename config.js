45d2be4db5c6bc036a15933d2ce3133556094b3a
tegbot76-source <tegbot76@gmail.com>
2025-08-20 16:24:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'Manu-MD&TZcSXISJ#RY3mbyqpIEYmVzF_sdtwJnf-whJqHFDWOtfQ_pksn4c', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000',
-MONGODB: process.env.MONGODB || 'put_your_mongodb_url' // අනිවාරෙන් Mongodb Url එකක් ඕනි දන්නෙ නැත්නම් මගෙ වීඩියෝ එක බලලා හදාගන්න.🔥
+MONGODB: process.env.MONGODB || 'mongodb+srv://anjubottest:Mali2006@cluster0.fznddsb.mongodb.net/' // අනිවාරෙන් Mongodb Url එකක් ඕනි දන්නෙ නැත්නම් මගෙ වීඩියෝ එක බලලා හදාගන්න.🔥
 };
